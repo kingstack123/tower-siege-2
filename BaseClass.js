2a1c3ccfaba9c29c311752f88408f4fdc70823f6
kingstack123 <72551699+kingstack123@users.noreply.github.com>
2021-06-21 16:43:27 +0530

    Add files via upload

--- BaseClass.js
+++ BaseClass.js
@@ -14,6 +14,7 @@ class BaseClass{
   }  
       
   display(){
+    if (this.body.speed < 3) {
     var angle = this.body.angle;
     fill("white")
     //console.log("angle=" + angle + " pos x=" + this.body.position.x + " pos y=" + this.body.position.y);
@@ -23,14 +24,13 @@ class BaseClass{
     rectMode(CENTER);
     rect(0, 0, this.width, this.height);
     pop();
-
+    }
     //console.log("speed=" + this.body.speed);
 
-    if (this.body.speed > 3) {
+    else {
       //console.log("visibility=" + this.visibility);
-   
-      push ();
       World.remove(world,this.body);
+      push ();
       this.visibility = this.visibility-5;
       tint(255,this.visibility);
       pop ();
--- Box.js
+++ Box.js
@@ -5,4 +5,11 @@ class Box extends BaseClass{
   }
    
 
+  
 };
+
+
+
+
+
+
